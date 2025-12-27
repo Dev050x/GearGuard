aa12cc6f2c54c626c25b1cac7cce2b05eec8ea50
Acharya Dhruv <acharyadhruv346@gmail.com>
2025-12-27 12:17:51 +0530

    feat: auth endpoints

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,1 +1,11 @@
-//authentication routes
+import { Router } from 'express';
+import { register , login } from '../controllers/auth.controller';
+import { validate } from '../middleware/validate.middleware';
+import { registerSchema, loginSchema } from '../schemas/auth.schema';
+
+const authRouter = Router();
+
+authRouter.post('/register', validate(registerSchema), register);
+authRouter.post('/login', validate(loginSchema), login);
+
+export default authRouter;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.routes';
+
 dotenv.config();
 
 const app = express();
@@ -8,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`server is listening on ${PORT}`);
