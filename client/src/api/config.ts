d56ddcdde878e0859b9088c38d682560a7e0f83c
0xDev05 <divpatel1274@gmail.com>
2025-12-27 15:47:25 +0530

    feat(client): add token interceptor using axios for token

---
+++ client/src/api/config.ts
@@ -0,0 +1,16 @@
+// api/config.ts
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/api';
+
+export const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('gearguard_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
